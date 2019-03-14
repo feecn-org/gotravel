700d9876422c66c244fdc475e0f6e3a13c22c775
pczhangyu <13717713687@163.com>
2019-03-14 09:05:24 +0800

    let's go travel with golang.

--- tool/http_request.go
+++ tool/http_request.go
@@ -9,14 +9,18 @@ import (
 )
 
 const (
+	ContentType     = "Content-Type"
 	ContentTypeJson = "application/json"
 	ContentTypeForm = "application/x-www-form-urlencoded"
+	MethodPost      = "POST"
+	MethodGet       = "Get"
+	Cookie          = "cookie"
 )
 
 /**
 http get request
 */
-func Http_get(getUrl string) string {
+func Http_Get(getUrl string) string {
 	resp, err := http.Get(getUrl)
 	if err != nil {
 	}
@@ -32,7 +36,7 @@ func Http_get(getUrl string) string {
 /**
 http post request json
 */
-func Http_post(postUrl string) string {
+func Http_Post(postUrl string) string {
 	resp, err := http.Post(postUrl, ContentTypeJson, strings.NewReader("body"))
 	if err != nil {
 	}
@@ -45,8 +49,11 @@ func Http_post(postUrl string) string {
 	return string(body)
 }
 
-func Http_post_form(postUrl string) string {
-	resp, err := http.PostForm("http://www.01happy.com/demo/accept.php",
+/**
+http post form
+*/
+func Http_Post_Form(postUrl string) string {
+	resp, err := http.PostForm(postUrl,
 		url.Values{"key": {"Value"}, "id": {"123"}})
 
 	if err != nil {
@@ -54,6 +61,34 @@ func Http_post_form(postUrl string) string {
 	}
 
 	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+	return string(body)
+}
+
+/**
+http request with header and cookie
+*/
+func httpDo(method string, urlStr string, header string, cookie string) string {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, urlStr, strings.NewReader(header))
+	if err != nil {
+		// handle error
+	}
+
+	req.Header.Set(ContentType, ContentTypeForm)
+	//req.Header.Set(Cookie, "name=anny")
+	req.Header.Set(Cookie, cookie)
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		// handle error
